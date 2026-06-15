75d444edea89b3a41d0513446162e5b6d23f5d86
Slawek-kw <slawek.kw@interia.pl>
2026-06-15 20:06:03 +0200

    Materialowka-MM

---
+++ baza_materialow.js
@@ -0,0 +1,16 @@
+const MOJA_BAZA_MATERIALOW = [
+    { name: "aaa", uwagi: "er" },
+    { name: "Butla z azotem", uwagi: "--" },
+    { name: "Butla z czynnikiem chłodniczym R32", uwagi: "--" },
+    { name: "Elektroniczna oprawa zaworowa TESTO", uwagi: "--" },
+    { name: "Miernik cęgowy Milwaukee", uwagi: "--" },
+    { name: "Milwaukee Packout - torba monterska", uwagi: "--" },
+    { name: "Pompa próżniowa Rothenberger", uwagi: "--" },
+    { name: "Stacja odzysku czynnika Rothenberger", uwagi: "--" },
+    { name: "Wiertarko-wkrętarka akumulatorowa Milwaukee", uwagi: "--" },
+    { name: "Wkrętak dielektryczny krzyżowy", uwagi: "--" },
+    { name: "Wkrętak dielektryczny płaski", uwagi: "--" },
+    { name: "wkrętaki", uwagi: "--" },
+    { name: "Zestaw kluczy imbusowych calowych", uwagi: "od 1/16" },
+    { name: "Zestaw kluczy imbusowych metrycznych", uwagi: "od 1.5mm - 10 mm" }
+];
